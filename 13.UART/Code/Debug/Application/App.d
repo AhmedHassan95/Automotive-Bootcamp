bc7c9f2d4d7d691bfde7f82b4c64349b5b59a46d
Ahmed Hassan Hamed Mohamed <ahmed.hassan9575@gmail.com>
2022-01-28 17:39:53 +0200

    Upload Files

---
+++ 13.UART/Code/Debug/Application/App.d
@@ -0,0 +1,24 @@
+Application/App.o Application/App.o: ../Application/App.c \
+  ../Application/App.h ../Application/../Library/std_types.h \
+  ../Application/../Library/common_macros.h \
+  ../Application/../Library/micro_config.h \
+  ../Application/../MCAL/UART/uart.h \
+  ../Application/../MCAL/UART/../../Library/std_types.h \
+  ../Application/../MCAL/UART/../../Library/common_macros.h \
+  ../Application/../MCAL/UART/../../Library/micro_config.h
+
+../Application/App.h:
+
+../Application/../Library/std_types.h:
+
+../Application/../Library/common_macros.h:
+
+../Application/../Library/micro_config.h:
+
+../Application/../MCAL/UART/uart.h:
+
+../Application/../MCAL/UART/../../Library/std_types.h:
+
+../Application/../MCAL/UART/../../Library/common_macros.h:
+
+../Application/../MCAL/UART/../../Library/micro_config.h:
