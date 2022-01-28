608eef67adefc63859d081fba96330383566c558
Ahmed Hassan Hamed Mohamed <ahmed.hassan9575@gmail.com>
2022-01-28 17:49:33 +0200

    Upload Files

---
+++ 14.Traffic Light/Code/Application/App.c
@@ -0,0 +1,95 @@
+ /******************************************************************************
+ *
+ * [MODULE]: APPLICATION
+ *
+ * [FILE NAME]: App.c
+ *
+ * [DESCRIPTION]: Source file for the Application
+ *
+ * [AUTHOR]: Ahmed Hassan
+ *
+ *******************************************************************************/
+
+#include "App.h"
+
+/*******************************************************************************
+ *                              Global Variables                               *
+ *******************************************************************************/
+
+uint8_t gStr_message[20];	/* Global string to be sent through uart */
+
+/*******************************************************************************
+ *                           Functions Definitions                             *
+ *******************************************************************************/
+
+/*******************************************************************************
+ * [Function Name]: App_init
+ *
+ * [Description]: 	Function to Initialize the application
+ *
+ * [Args]:			None
+ *
+ * [in]			  	None
+ *
+ * [out]		  	None
+ *
+ * [in/out]		 	None
+ *
+ * [Returns]:       None
+ *******************************************************************************/
+void App_init(void)
+{
+	LED_init();	/* Initialize LED Driver */
+	UART_ConfigType uart_config = {BPS_9600, NORMAL_SPEED, EIGHT_BITS, DISABLED, ONE_BIT};
+	UART_init(&uart_config);
+}
+
+/*******************************************************************************
+ * [Function Name]: App_Update
+ *
+ * [Description]: 	Function to update the state of the application
+ *
+ * [Args]:			None
+ *
+ * [in]			  	None
+ *
+ * [out]		  	None
+ *
+ * [in/out]		 	None
+ *
+ * [Returns]:       None
+ *******************************************************************************/
+void App_Update(void)
+{
+	UART_receiveString(gStr_message); /* Receive String from Terminal */
+
+	if(strcmp(gStr_message, "START") == IDENTICAL)
+	{
+		LED_turnOn(LED_GREEN);
+		LED_turnOff(LED_YELLOW);
+		LED_turnOff(LED_RED);
+		UART_sendString("Green LED is on\r");
+	}
+	else if(strcmp(gStr_message, "WAIT") == IDENTICAL)
+	{
+		LED_turnOn(LED_YELLOW);
+		LED_turnOff(LED_GREEN);
+		LED_turnOff(LED_RED);
+		UART_sendString("Yellow LED is on\r");
+	}
+	else if(strcmp(gStr_message, "STOP") == IDENTICAL)
+	{
+		LED_turnOn(LED_RED);
+		LED_turnOff(LED_YELLOW);
+		LED_turnOff(LED_GREEN);
+		UART_sendString("Red LED is on\r");
+	}
+	else if(strcmp(gStr_message, "AT") == IDENTICAL)
+	{
+		UART_sendString("OK\r");
+	}
+	else
+	{
+		/* No thing to do */
+	}
+}
---
+++ 14.Traffic Light/Code/HAL/LED/led.h
@@ -0,0 +1,110 @@
+ /******************************************************************************
+ *
+ * [MODULE]: LED
+ *
+ * [FILE NAME]: led.h
+ *
+ * [DESCRIPTION]: Header file for the LED Driver
+ *
+ * [AUTHOR]: Ahmed Hassan
+ *
+ *******************************************************************************/
+
+#ifndef LED_H_
+#define LED_H_
+
+#include "../../Library/std_types.h"
+#include "../../Library/common_macros.h"
+#include "../../Library/micro_config.h"
+#include "../../MCAL/DIO/dio.h"
+
+/*******************************************************************************
+ *                             Preprocessor Macros                             *
+ *******************************************************************************/
+
+/* LED Pin Configurations */
+#define LED_PORT_NUM 	   DIO_PORTB
+#define LED_NUM			   4
+
+/******************************************************************************
+ *                         Types Declaration                                  *
+ ******************************************************************************/
+
+typedef enum
+{
+	LED_0, LED_1, LED_2
+}enuLED_Id_t;
+
+/*******************************************************************************
+ *                             Functions Prototypes                            *
+ *******************************************************************************/
+
+/*******************************************************************************
+ * [Function Name]: LED_init
+ *
+ * [Description]: 	Function to Initialize the LED Driver
+ *
+ * [Args]:			None
+ *
+ * [in]			  	None
+ *
+ * [out]		  	None
+ *
+ * [in/out]		 	None
+ *
+ * [Returns]:       None
+ *******************************************************************************/
+void LED_init(void);
+
+/*******************************************************************************
+ * [Function Name]: LED_turnOn
+ *
+ * [Description]: 	Function to turn on the LED
+ *
+ * [Args]:			enu_LedId
+ *
+ * [in]			  	enu_LedId: Enumerator to LED ID
+ *
+ * [out]		  	None
+ *
+ * [in/out]		 	None
+ *
+ * [Returns]:       None
+ *******************************************************************************/
+void LED_turnOn(const enuLED_Id_t enu_LedId);
+
+/*******************************************************************************
+ * [Function Name]: LED_turnOff
+ *
+ * [Description]: 	Function to turn off the LED
+ *
+ * [Args]:			enu_LedId
+ *
+ * [in]			  	enu_LedId: Enumerator to LED ID
+ *
+ * [out]		  	None
+ *
+ * [in/out]		 	None
+ *
+ * [Returns]:       None
+ *******************************************************************************/
+void LED_turnOff(const enuLED_Id_t enu_LedId);
+
+/*******************************************************************************
+ * [Function Name]: LED_toggle
+ *
+ * [Description]: 	Function to toggle the LED
+ *
+ * [Args]:			enu_LedId
+ *
+ * [in]			  	enu_LedId: Enumerator to LED ID
+ *
+ * [out]		  	None
+ *
+ * [in/out]		 	None
+ *
+ * [Returns]:       None
+ *******************************************************************************/
+void LED_toggle(const enuLED_Id_t enu_LedId);
+
+#endif /* LED_H_ */
