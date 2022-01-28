55e52ce50a7181e9c015e84e973d2ab2c7807b2b
Ahmed Hassan Hamed Mohamed <ahmed.hassan9575@gmail.com>
2022-01-28 18:25:05 +0200

    Upload Files

---
+++ 10.LCD/Code/Application/App.h
@@ -0,0 +1,43 @@
+ /******************************************************************************
+ *
+ * [MODULE]: APPLICATION
+ *
+ * [FILE NAME]: App.h
+ *
+ * [DESCRIPTION]: Header file for the Application
+ *
+ * [AUTHOR]: Ahmed Hassan
+ *
+ *******************************************************************************/
+
+#ifndef APP_H_
+#define APP_H_
+
+#include "../Library/std_types.h"
+#include "../Library/common_macros.h"
+#include "../Library/micro_config.h"
+#include "../HAL/LCD/lcd.h"
+#include "../MCAL/dio.h"
+
+/*******************************************************************************
+ *                             Functions Prototypes                            *
+ *******************************************************************************/
+
+/*******************************************************************************
+ * [Function Name]: App_init
+ *
+ * [Description]: 	Function to Initialize the application to test lcd
+ *
+ * [Args]:			None
+ *
+ * [in]			  	None
+ *
+ * [out]		  	None
+ *
+ * [in/out]		 	None
+ *
+ * [Returns]:       None
+ *******************************************************************************/
+void App_init(void);
+
+#endif /* APP_H_ */
