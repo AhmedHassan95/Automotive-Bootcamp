a3ee00b6ccb1900e716ecb3bf736649da6b0f401
Ahmed Hassan <ahmed.hassan9575@gmail.com>
2022-01-29 15:09:27 +0200

    Update led.h

--- 14.Traffic Light/Code/HAL/LED/led.h
+++ 14.Traffic Light/Code/HAL/LED/led.h
@@ -42,68 +42,68 @@ typedef enum
 /*******************************************************************************
  * [Function Name]: LED_init
  *
- * [Description]: 	Function to Initialize the LED Driver
+ * [Description]: Function to Initialize the LED Driver
  *
- * [Args]:			None
+ * [Args]:	  None
  *
- * [in]			  	None
+ * [in]		  None
  *
- * [out]		  	None
+ * [out]	  None
  *
- * [in/out]		 	None
+ * [in/out]	  None
  *
- * [Returns]:       None
+ * [Returns]:     None
  *******************************************************************************/
 void LED_init(void);
 
 /*******************************************************************************
  * [Function Name]: LED_turnOn
  *
- * [Description]: 	Function to turn on the LED
+ * [Description]: Function to turn on the LED
  *
- * [Args]:			enu_LedId
+ * [Args]:	  enu_LedId
  *
- * [in]			  	enu_LedId: Enumerator to LED ID
+ * [in]		  enu_LedId: Enumerator to LED ID
  *
- * [out]		  	None
+ * [out]	  None
  *
- * [in/out]		 	None
+ * [in/out]	  None
  *
- * [Returns]:       None
+ * [Returns]:     None
  *******************************************************************************/
 void LED_turnOn(const enuLED_Id_t enu_LedId);
 
 /*******************************************************************************
  * [Function Name]: LED_turnOff
  *
- * [Description]: 	Function to turn off the LED
+ * [Description]: Function to turn off the LED
  *
- * [Args]:			enu_LedId
+ * [Args]:	  enu_LedId
  *
- * [in]			  	enu_LedId: Enumerator to LED ID
+ * [in]		  enu_LedId: Enumerator to LED ID
  *
- * [out]		  	None
+ * [out]	  None
  *
- * [in/out]		 	None
+ * [in/out]	  None
  *
- * [Returns]:       None
+ * [Returns]:     None
  *******************************************************************************/
 void LED_turnOff(const enuLED_Id_t enu_LedId);
 
 /*******************************************************************************
  * [Function Name]: LED_toggle
  *
- * [Description]: 	Function to toggle the LED
+ * [Description]: Function to toggle the LED
  *
- * [Args]:			enu_LedId
+ * [Args]:	  enu_LedId
  *
- * [in]			  	enu_LedId: Enumerator to LED ID
+ * [in]		  enu_LedId: Enumerator to LED ID
  *
- * [out]		  	None
+ * [out]	  None
  *
- * [in/out]		 	None
+ * [in/out]	  None
  *
- * [Returns]:       None
+ * [Returns]:     None
  *******************************************************************************/
 void LED_toggle(const enuLED_Id_t enu_LedId);
 
