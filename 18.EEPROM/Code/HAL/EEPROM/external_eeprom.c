15c086992e82e7d3972e052551020f0e63e1beb1
Ahmed Hassan <ahmed.hassan9575@gmail.com>
2022-01-29 12:39:59 +0200

    Update external_eeprom.c

--- 18.EEPROM/Code/HAL/EEPROM/external_eeprom.c
+++ 18.EEPROM/Code/HAL/EEPROM/external_eeprom.c
@@ -14,23 +14,23 @@
 #include "external_eeprom.h"
 
 /******************************************************************************************
- *                  		      Functions Definitions                                   *
+ *                  		      Functions Definitions                               *
  ******************************************************************************************/
 
 /******************************************************************************************
  * [Function Name]: EEPROM_init
  *
- * [Description]: 	Function responsible for initializing EEPROM driver
+ * [Description]: Function responsible for initializing EEPROM driver
  *
- * [Args]:			None
+ * [Args]:	None
  *
- * [in]			  	None
+ * [in]		None
  *
- * [out]		  	None
+ * [out]	None
  *
- * [in/out]		 	None
+ * [in/out]	None
  *
- * [Returns]:       None
+ * [Returns]:   None
  ******************************************************************************************/
 void EEPROM_init(void)
 {
@@ -44,18 +44,18 @@ void EEPROM_init(void)
 /******************************************************************************************
  * [Function Name]: EEPROM_writeByte
  *
- * [Description]: 	Function responsible for writing 1 Byte in the EEPROM
+ * [Description]: Function responsible for writing 1 Byte in the EEPROM
  *
- * [Args]:			u16addr, u8data
+ * [Args]:	u16addr, u8data
  *
- * [in]			  	u16addr: Unsigned Short (EEPROM Byte Address)
- * 					u8data:	 Unsigned Character (Data need to be written in the EEPROM)
+ * [in]		u16addr: Unsigned Short (EEPROM Byte Address)
+ * 		u8data:	 Unsigned Character (Data need to be written in the EEPROM)
  *
- * [out]		  	Unsigned Character
+ * [out]	Unsigned Character
  *
- * [in/out]		 	None
+ * [in/out]	None
  *
- * [Returns]:       Status of this function (SUCCESS, ERROR)
+ * [Returns]:   Status of this function (SUCCESS, ERROR)
  ******************************************************************************************/
 uint8_t EEPROM_writeByte(uint16_t u16addr, uint8_t u8data)
 {
@@ -91,18 +91,18 @@ uint8_t EEPROM_writeByte(uint16_t u16addr, uint8_t u8data)
 /******************************************************************************************
  * [Function Name]: EEPROM_readByte
  *
- * [Description]: 	Function responsible for reading 1 Byte from the EEPROM
+ * [Description]: Function responsible for reading 1 Byte from the EEPROM
  *
- * [Args]:			u16addr, u8data
+ * [Args]:	u16addr, u8data
  *
- * [in]			  	u16addr: Unsigned Short (Address where the data will be read from the EEPROM)
- * 					u8data: Pointer to Character (The data that will read from the EEPROM)
+ * [in]		u16addr: Unsigned Short (Address where the data will be read from the EEPROM)
+ * 		u8data: Pointer to Character (The data that will read from the EEPROM)
  *
- * [out]		  	Unsigned Character
+ * [out]	Unsigned Character
  *
- * [in/out]		 	None
+ * [in/out]	None
  *
- * [Returns]:       Status of this function (SUCCESS, ERROR)
+ * [Returns]:   Status of this function (SUCCESS, ERROR)
  ******************************************************************************************/
 uint8_t EEPROM_readByte(uint16_t u16addr, uint8_t * u8data)
 {
