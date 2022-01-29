061af71468a92d41d642b197e45b604d35b9eb14
Ahmed Hassan <ahmed.hassan9575@gmail.com>
2022-01-29 16:02:39 +0200

    Update App.c

--- 16.Simple Chatting System/Code/MCU1/Application/App.c
+++ 16.Simple Chatting System/Code/MCU1/Application/App.c
@@ -25,17 +25,17 @@ uint8_t gStr_message[20];	/* Global string to be sent through the spi */
 /*******************************************************************************
  * [Function Name]: App_init
  *
- * [Description]: 	Function to Initialize the application
+ * [Description]: Function to Initialize the application
  *
- * [Args]:			None
+ * [Args]:	  None
  *
- * [in]			  	None
+ * [in]		  None
  *
- * [out]		  	None
+ * [out]	  None
  *
- * [in/out]		 	None
- *
- * [Returns]:       None
+ * [in/out]	  None
+ *  
+ * [Returns]:     None
  *******************************************************************************/
 void App_init(void)
 {
@@ -49,23 +49,23 @@ void App_init(void)
 /*******************************************************************************
  * [Function Name]: App_Update
  *
- * [Description]: 	Function to update the state of the application
+ * [Description]: Function to update the state of the application
  *
- * [Args]:			None
+ * [Args]:	  None
  *
- * [in]			  	None
+ * [in]		  None
  *
- * [out]		  	None
+ * [out]	  None
  *
- * [in/out]		 	None
+ * [in/out]	  None
  *
- * [Returns]:       None
+ * [Returns]:     None
  *******************************************************************************/
 void App_Update(void)
 {
 	 UART_receiveString(gStr_message);	/* Receive string from Terminal */
 
-	 SPI_sendByte(M2_READY);			/* Check if Slave MCU is ready */
+	 SPI_sendByte(M2_READY);		/* Check if Slave MCU is ready */
 
 	 SPI_sendString(gStr_message);		/* Send string to Slave MCU */
 }
