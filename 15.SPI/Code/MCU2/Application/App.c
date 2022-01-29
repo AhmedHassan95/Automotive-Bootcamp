0ee717a576079343c90a40a601db1e4959550f81
Ahmed Hassan <ahmed.hassan9575@gmail.com>
2022-01-29 15:42:13 +0200

    Update App.c

--- 15.SPI/Code/MCU2/Application/App.c
+++ 15.SPI/Code/MCU2/Application/App.c
@@ -16,7 +16,7 @@
  *                              Global Variables                               *
  *******************************************************************************/
 
-uint8_t gu8_Byte;			/* Global variable to store the value of the sent byte */
+uint8_t gu8_Byte;		/* Global variable to store the value of the sent byte */
 uint8_t gStr_message[20];	/* Global string to be received through spi */
 
 /*******************************************************************************
@@ -26,27 +26,27 @@ uint8_t gStr_message[20];	/* Global string to be received through spi */
 /*******************************************************************************
  * [Function Name]: App_init
  *
- * [Description]: 	Function to Initialize the application to test spi Driver
+ * [Description]: Function to Initialize the application to test spi Driver
  *
- * [Args]:			None
+ * [Args]:	  None
  *
- * [in]			  	None
+ * [in]		  None
  *
- * [out]		  	None
+ * [out]	  None
  *
- * [in/out]		 	None
+ * [in/out]	  None
  *
- * [Returns]:       None
+ * [Returns]:     None
  *******************************************************************************/
 void App_init(void)
 {
-	LCD_init();			/* Initialize LCD Driver */
+	LCD_init();		/* Initialize LCD Driver */
 	SPI_initSlave();	/* Initialize Slave SPI */
 
-	gu8_Byte = SPI_receiveByte();		/* Receive Character from Master MCU */
-	LCD_displayCharacter(gu8_Byte);		/* Display the received string on the LCD display */
+	gu8_Byte = SPI_receiveByte();	/* Receive Character from Master MCU */
+	LCD_displayCharacter(gu8_Byte);	/* Display the received string on the LCD display */
 
-	SPI_receiveString(gStr_message);			/* Receive String from Master MCU */
+	SPI_receiveString(gStr_message);		/* Receive String from Master MCU */
 	LCD_displayStringRowColumn(1, 0, gStr_message);	/* Display the received string on the LCD display */
 }
 
