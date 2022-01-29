f596a3d12b6ef8e11554c612c9a2010f57298968
Ahmed Hassan <ahmed.hassan9575@gmail.com>
2022-01-29 16:05:16 +0200

    Update App.h

--- 16.Simple Chatting System/Code/MCU1/Application/App.h
+++ 16.Simple Chatting System/Code/MCU1/Application/App.h
@@ -50,11 +50,11 @@ void App_init(void);
  *
  * [Args]:	None
  *
- * [in]	      None
+ * [in]	        None
  *
- * [out]      None
+ * [out]        None
  *
- * [in/out]    None
+ * [in/out]     None
  *
  * [Returns]:    None
  *******************************************************************************/
