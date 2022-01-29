54f95456f36c4fcfc2f75bc43af48d6c030244a7
Ahmed Hassan <ahmed.hassan9575@gmail.com>
2022-01-29 16:59:15 +0200

    Update App.h

--- 17.I2C/Code/MCU1/Application/App.h
+++ 17.I2C/Code/MCU1/Application/App.h
@@ -35,11 +35,11 @@
  *
  * [in]		None
  *
- * [out]		None
+ * [out]	None
  *
  * [in/out]	None
  *
- * [Returns]:    None
+ * [Returns]:   None
  *******************************************************************************/
 void App_init(void);
 
@@ -50,9 +50,9 @@ void App_init(void);
  *
  * [Args]:	None
  *
- * [in]			None
+ * [in]		None
  *
- * [out]		None
+ * [out]	None
  *
  * [in/out]	None
  *
