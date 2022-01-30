b1bcd59a0db97bcff05ed006ec0ab04e3161b5b8
Ahmed Hassan <ahmed.hassan9575@gmail.com>
2022-01-30 11:47:01 +0200

    Update App.h

--- 15.SPI/Code/MCU1/Application/App.h
+++ 15.SPI/Code/MCU1/Application/App.h
@@ -28,15 +28,15 @@
  *
  * [Description]: Function to Initialize the application to test spi Driver
  *
- * [Args]:	None
+ * [Args]:	  None
  *
- * [in]		None
+ * [in]		  None
  *
- * [out]	None
+ * [out]	  None
  *
- * [in/out]	None
+ * [in/out]	  None
  *
- * [Returns]:   None
+ * [Returns]:     None
  *******************************************************************************/
 void App_init(void);
 
