1d54266e11b580415c83529c8d05a387b8188daa
Ahmed Hassan <ahmed.hassan9575@gmail.com>
2022-01-30 15:40:43 +0200

    Update keypad_test.c

--- 9.Keypad/Code/keypad_test.c
+++ 9.Keypad/Code/keypad_test.c
@@ -25,7 +25,7 @@ int main(void)
 	App_init();	/* Test KEYPAD in simple application */
 
 	/********************************************************************************
-	 *                        APPLICATION	(SUPER LOOP)						    *
+	 *                        APPLICATION	(SUPER LOOP)			        *
 	 ********************************************************************************/
 
 	while(1)
