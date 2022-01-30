d57892fea3274151a439484c43c970a180432b25
Ahmed Hassan <ahmed.hassan9575@gmail.com>
2022-01-30 13:25:27 +0200

    Update button.h

--- Task6/Code/HAL/button.h
+++ Task6/Code/HAL/button.h
@@ -40,11 +40,11 @@
  *
  * [Args]:	  None
  *
- * [in]		 None
+ * [in]		  None
  *
- * [out]	None
+ * [out]	  None
  *
- * [in/out]	None
+ * [in/out]	 None
  *
  * [Returns]:      None
  *******************************************************************************/
