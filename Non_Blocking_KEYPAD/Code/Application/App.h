540a5e6ee5206a888f3612042467af000119c7d6
Ahmed Hassan <ahmed.hassan9575@gmail.com>
2022-02-11 15:42:10 +0200

    Update App.h

--- Non_Blocking_KEYPAD/Code/Application/App.h
+++ Non_Blocking_KEYPAD/Code/Application/App.h
@@ -27,34 +27,34 @@
 /*******************************************************************************
  * [Function Name]: App_init
  *
- * [Description]: 	Function to Initialize the application to test keypad
+ * [Description]: Function to Initialize the application to test keypad
  *
- * [Args]:			None
+ * [Args]:	None
  *
- * [in]			  	None
+ * [in]			None
  *
- * [out]		  	None
+ * [out]		None
  *
- * [in/out]		 	None
+ * [in/out]	None
  *
- * [Returns]:       None
+ * [Returns]:  None
  *******************************************************************************/
 void App_init(void);
 
 /*******************************************************************************
  * [Function Name]: App_Update
  *
- * [Description]: 	Function to update the state of the application
+ * [Description]: Function to update the state of the application
  *
- * [Args]:			None
+ * [Args]:	None
  *
- * [in]			  	None
+ * [in]			None
  *
- * [out]		  	None
+ * [out]		None
  *
- * [in/out]		 	None
+ * [in/out]	None
  *
- * [Returns]:       None
+ * [Returns]:  None
  *******************************************************************************/
 void App_Update(void);
 
