856a78c7b89009bbf7232af928e379467d05d956
Ahmed Hassan <ahmed.hassan9575@gmail.com>
2022-02-11 15:44:15 +0200

    Update App.h

--- Non_Blocking_KEYPAD/Code/Application/App.h
+++ Non_Blocking_KEYPAD/Code/Application/App.h
@@ -31,13 +31,13 @@
  *
  * [Args]:	 None
  *
- * [in]         None
+ * [in]          None
  *
  * [out]	 None
  *
  * [in/out]	 None
  *
- * [Returns]:   None
+ * [Returns]:    None
  *******************************************************************************/
 void App_init(void);
 
