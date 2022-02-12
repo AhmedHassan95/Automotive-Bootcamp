a5d36d1d38ecdd8c9de3d01067aaf3c498b804b7
Ahmed Hassan <ahmed.hassan9575@gmail.com>
2022-02-12 17:57:24 +0200

    Update App.h

--- Non_Blocking_KEYPAD/Code/Application/App.h
+++ Non_Blocking_KEYPAD/Code/Application/App.h
@@ -29,13 +29,13 @@
  *
  * [Description]: Function to Initialize the application to test keypad
  *
- * [Args]:			     None
+ * [Args]:			 None
  *
- * [in]			  	     None
+ * [in]			  	 None
  *
- * [out]		  	     None
+ * [out]		  	 None
  *
- * [in/out]		 	   None
+ * [in/out]		 None
  *
  * [Returns]:     None
  *******************************************************************************/
